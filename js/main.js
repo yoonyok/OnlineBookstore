ea96cc5826162fade57cd973d19d2fab8e486c2d
Colby Song <colbysong@yahoo.ca>
2017-10-13 15:39:24 -0700

    new products global, move images to products

--- js/main.js
+++ js/main.js
@@ -1,35 +1,59 @@
-var productsNameList = [
-    "Box1",
-    "Box2",
-    "Clothes1",
-    "Clothes2",
-    "Jeans",
-    "Keyboard",
-    "KeyboardCombo",
-    "Mice",
-    "PC1",
-    "PC2",
-    "PC3",
-    "Tent"
-];
+var products = {
+    'Box1': {
+        'product': new Product('Box1', 10, 'images/products/Box1_$10.png'),
+        'quantity': 5
+    },
+    'Box2': {
+        'product': new Product('Box2', 5, 'images/products/Box1_$5.png'),
+        'quantity': 5
+    },
+    'Clothes1': {
+        'product': new Product('Clothes1', 20, 'images/products/Clothes1_$20.png'),
+        'quantity': 5
+    },
+    'Clothes2': {
+        'product': new Product('Clothes2', 30, 'images/products/Clothes2_$30.png'),
+        'quantity': 5
+    },
+    'Jeans': {
+        'product': new Product('Jeans', 50, 'images/products/Jeans_$50.png'),
+        'quantity': 5
+    },
+    'Keyboard': {
+        'product': new Product('Keyboard', 20, 'images/products/Keyboard_$20.png'),
+        'quantity': 5
+    },
+    'KeyboardCombo': {
+        'product': new Product('KeyboardCombo', 40, 'images/products/KeyboardCombo_$40.png'),
+        'quantity': 5
+    },
+    'Mice': {
+        'product': new Product('Mice', 20, 'images/products/Mice_$20.png'),
+        'quantity': 5
+    },
+    'PC1': {
+        'product': new Product('PC1', 350, 'images/products/PC1_$350.png'),
+        'quantity': 5
+    },
+    'PC2': {
+        'product': new Product('PC2',400, 'images/products/PC2_$400.png'),
+        'quantity': 5
+    },
+    'PC3': {
+        'product': new Product('PC3', 300, 'images/products/PC3_$300.png'),
+        'quantity': 5
+    },
+    'Tent': {
+        'product': new Product('Tent', 100, 'images/products/Tent_$100.png'),
+        'quantity': 5
+    }
+};
 var cart = {};
-var products = {};
 var inactiveTime = 0;  // users inactive time in seconds
-var alertTime = 30000;
+var alertTime = 300000;
 var alertUserTimerId;
 var trackInactiveTimeId;
 
-// used to product objects from name list
-// this way if we were to add a product, we only have to add the name
-// to the productsNameList
-function constructObject(object, field, value) {
-    object[field] = value;
-}
-
-// construct product global variables
-for (var i = 0, len = productsNameList.length; i < len; i++) {
-    constructObject(products, productsNameList[i], 5);
-}
 
 // Adds a given product into the cart and decrease its supply value by 1
 function addToCart(productName) {
